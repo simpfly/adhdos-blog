3ed3eae41edda31fdbe1fac9532f540337b357d9
simpfly <zyr5201999@qq.com>
2026-06-23 10:00:49 +0800

    fix: correctly extract lang from root pathname

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,7 +2,7 @@ import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
   const parts = url.pathname.split('/');
-  const lang = parts[2];
+  const lang = parts[1];
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
